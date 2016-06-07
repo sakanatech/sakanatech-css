39088e759fb869740113a7b4900c1dd3ea41192c
lyca <l92.168.50.182@gmail.com>
2016-06-07 17:40:09 +0900

    不要ファイル削除

--- Sass/config.rb
+++ Sass/config.rb
@@ -1,6 +1,8 @@
 http_path = "/"
-css_dir = "/"
-sass_dir = "Sass"
+css_dir = "../"
+sass_dir = "/"
 images_dir = "/"
 javascripts_dir = "/"
+cache = false
+cache_dir = "../../sass-cache"
 Encoding.default_external = 'utf-8'
